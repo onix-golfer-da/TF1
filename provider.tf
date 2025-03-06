594f72d91a05bfaa2f1ec1a9ba407dd92d2399be
Dave Alberto <david.alberto@onixnet.com>
2025-03-06 10:50:03 -0500

    gcp info

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "my-project-id"
-  region  = "us-central1"
-  # credentials = GOOGLE_APPLICATION_CREDENTIALS
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
